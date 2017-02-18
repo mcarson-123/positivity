3817b0ef7bc4dce2397cb972cb666636214f98d5
Madeline Carson <madeline.carson@gmail.com>
2017-02-18 09:13:50 -0800

    Add functionality to consume start/stop texts from user

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,43 @@
+class TwilioController < ApplicationController
+  before_action :ensure_twilio_sender
+
+  def incoming_message
+    body = params["Body"]
+    case body.strip.downcase
+    when "confirm"
+      @subscription = Subscriptions::Confirm.call(params)
+      if @subscription.try(:active?)
+        respond = true
+        message = "Thanks for confirming, positive messages are coming your way soon..."
+      end
+    when "stop"
+      @subscriptions = Subscriptions::Unsubscribe.call(params, from_twilio: true)
+      # Can't respond with a message since twilio won't send messages after
+      # receiving a "STOP"
+    when "start"
+      @subscription = Subscriptions::Confirm.call(params)
+      if @subscription.try(:active?)
+        respond = true
+        message = "Welcome back! Positive messages are coming your way soon..."
+      end
+    end
+
+    if respond
+      twiml = Twilio::TwiML::Response.new do |r|
+        r.Message message
+      end
+      render xml: twiml.text and return
+    end
+
+    # Else need to let Twilio know there is nothing to do
+    head :no_content
+  end
+
+  private
+
+  def ensure_twilio_sender
+    unless params["AccountSid"] == ENV["TWILIO_ACCOUNT_SID"]
+      render json: { errors: { request: ["Source may not confirm Subscription"] } }, status: :forbidden
+    end
+  end
+end
--- app/services/subscriptions/unsubscribe.rb
+++ app/services/subscriptions/unsubscribe.rb
@@ -2,10 +2,15 @@ module Subscriptions
   module Unsubscribe
     extend self
 
-    def call(params)
-      params = Subscriptions::CoercePhoneNumberParams.call(params)
+    def call(params, from_twilio: false)
+      phone_number =
+        if from_twilio
+          params["From"]
+        else # from confirmations controller
+          Subscriptions::CoercePhoneNumberParams.call(params)["phone_number"]
+        end
 
-      subscriptions = Subscription.where(phone_number: params[:phone_number])
+      subscriptions = Subscription.where(phone_number: phone_number)
       subscriptions.update_all(activation_state: Subscription::ActivationState::INACTIVE)
       subscriptions
     end
